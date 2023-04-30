077c0f2ad038d73d5a1c1d42c93ed799dbba23ed
Mohammed Sayed <farsfhd123@gmail.com>
2023-04-30 19:36:03 +0300

    List students and their results and finish

--- src/app/doctor/components/students/students.component.ts
+++ src/app/doctor/components/students/students.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.scss']
+  styleUrls: ['./students.component.scss'],
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit {
+  dataSource: any;
+  dataTable: any;
+  displayedColumns: any;
+  constructor(private service: AuthService) {
+    this.displayedColumns = ['position', 'name', 'subjectName', 'degree'];
+  }
+  ngOnInit(): void {
+    this.getStudents();
+  }
 
+  getStudents() {
+    this.service.getUsers('students').subscribe((res: any) => {
+      this.dataSource = res?.map((student: any) => {
+        if (student?.subjects) {
+          return student?.subjects?.map((sub: any) => {
+            return {
+              name: student.username,
+              subjectName: sub.name,
+              degree: sub.degree,
+            };
+          });
+        } else {
+          return [
+            {
+              name: student.username,
+              subjectName: '-',
+              degree: '-',
+            },
+          ];
+        }
+      });
+      this.dataTable = [];
+      this.dataSource.forEach((item: any) => {
+        item.forEach((subItem: any) => {
+          this.dataTable.push({
+            name: subItem.name,
+            subjectName: subItem.subjectName,
+            degree: subItem.degree,
+          });
+        });
+      });
+    });
+  }
 }
